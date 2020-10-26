d3d97a80a33e5e9f6210fee71dc7675ffbce4255
Gbenga <omg.tosine@gmail.com>
2020-10-26 14:37:22 +0100

    concurrently run queries to google's api endpoint

--- query.go
+++ query.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -23,7 +25,7 @@ func (q *Query) find(types string) (*response, error) {
 
 	values, err := buildQuery(q, types)
 	if err != nil {
-		log.Println("Error Parsing Price Range: ", err)
+		log.Println(" Error building query: ", err)
 		return nil, err
 	}
 
@@ -46,6 +48,7 @@ func (q *Query) find(types string) (*response, error) {
 		}
 		break
 	}
+	// fmt.Println(result)
 	return result, nil
 }
 
@@ -59,6 +62,7 @@ func buildQuery(q *Query, types string) (url.Values, error) {
 		// TODO: parse the price range
 		r, err := ParsePriceRange(q.PriceRange)
 		if err != nil {
+			log.Println("Error parsing price range")
 			return nil, err
 		}
 		values.Set("minprice", fmt.Sprintf("%d", int(r.From)-1))
@@ -69,9 +73,10 @@ func buildQuery(q *Query, types string) (url.Values, error) {
 
 func makeRequest(uri *url.URL) (*http.Response, error) {
 	// TODO: implement retries
-	timeout := time.Duration(10 * time.Second)
+	// set a timeout and initialize http client
+	// timeout := time.Duration(10 * time.Second)
 	client := http.Client{
-		Timeout: timeout,
+		// Timeout: timeout,
 	}
 	request, err := http.NewRequest("GET", uri.String(), nil)
 	if err != nil {
@@ -80,4 +85,83 @@ func makeRequest(uri *url.URL) (*http.Response, error) {
 	}
 	request.Header.Set("Content-type", "application/json")
 	return client.Do(request)
+}
+
+// Run makes concurrent queries to Google's servers
+func (q *Query) Run() []interface{} {
+	var w sync.WaitGroup
+	places := make([]interface{}, len(q.Destinations))
+	rand.Seed(time.Now().UnixNano())
+	var l sync.Mutex
+	// placechan := make(chan []interface{})
+	for i, dst := range q.Destinations {
+		w.Add(1)
+		// go searchPlace(q, dst, i, &w, placechan)
+		go func(types string, i int) {
+			defer w.Done()
+			// get the result from google's api endpoint query
+			res, err := q.find(types)
+			if err != nil {
+				log.Println("Failed to find places:", err)
+				return
+			}
+			// check for found places
+			if len(res.Results) == 0 {
+				log.Println("No result found for ", types)
+				return
+			}
+			// load the photos of found places
+			for _, result := range res.Results {
+				for _, photo := range result.Photos {
+					photo.URL = photosURL + "maxwidth=1000&photoreference=" + photo.PhotoRef + "&key=" + APIKey
+				}
+			}
+			// create a random integer to randomize picks
+			randI := rand.Intn(len(res.Results))
+			l.Lock()
+			// log.Println(res.Results)
+			fmt.Println(randI)
+			// for randI = range res.Results {
+			// 	places = append(places, res.Results[randI])
+			// }
+			places[i] = res.Results[randI]
+			l.Unlock()
+		}(dst, i)
+	}
+	w.Wait()
+	fmt.Println("run")
+	return places
+}
+
+func searchPlace(q *Query, types string, i int, w *sync.WaitGroup, c chan []interface{}) {
+	// create a mutual exclusion lock to allow the goroutines access the shared varible (the map) concurrently
+	places := make([]interface{}, len(q.Destinations))
+	rand.Seed(time.Now().UnixNano())
+	var l sync.Mutex
+	defer w.Done()
+	// get the result from google's api endpoint query
+	res, err := q.find(types)
+	if err != nil {
+		log.Println("Failed to find places:", err)
+		return
+	}
+	// check for found places
+	if len(res.Results) == 0 {
+		log.Println("No result found for ", types)
+		return
+	}
+	// load the photos of found places
+	for _, result := range res.Results {
+		for _, photo := range result.Photos {
+			photo.URL = photosURL + "maxwidth=1000&photoreference=" + photo.PhotoRef + "&key=" + APIKey
+		}
+	}
+	// create a random integer to randomize picks
+	randI := rand.Intn(len(res.Results))
+	l.Lock()
+	// log.Println(res.Results)
+	places[i] = res.Results[randI]
+	c <- places
+	close(c)
+	l.Unlock()
 }
